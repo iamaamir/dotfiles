78ece9b7d15eeaff495f3411a4bbc85c7df1b044
iamaamir <aamirkhan180@gmail.com>
2022-11-17 01:23:08 +0530

    raycast added

---
+++ raycast/current-time.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Current Time
+# @raycast.mode inline
+# @raycast.refreshTime 06s
+
+# Optional parameters:
+# @raycast.icon ⏰
+# @raycast.packageName showtime
+
+# Documentation:
+# @raycast.description show the current time
+# @raycast.author Aamir
+# @raycast.authorURL github.com/iamaamir
+
+date +"%r"
+
---
+++ raycast/localip.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title localip
+# @raycast.mode inline
+# @raycast.refreshTime 1h
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName IP
+
+# Documentation:
+# @raycast.description show the local ip address
+# @raycast.author Aamir
+# @raycast.authorURL github.com/iamaamir
+
+
+
+localip
